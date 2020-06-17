ce4355dc717ac9527fef91c3e639066d58d85302
Kenny <kpstanleyjr@gmail.com>
2020-06-17 18:09:37 -0500

    improved responsiveness

--- request.js
+++ request.js
@@ -1,6 +1,7 @@
 const input = document.getElementById("search-box");
 let q = '';
 let button = false;
+let errorCounter = 0;
 
 function displayGif(inputText) {
     if (inputText) {
@@ -12,21 +13,21 @@ function displayGif(inputText) {
     if (button) {
         params = "tag=" + document.getElementById("search-box").value;
     }
-    console.log(params);
     
     const apiKey = "api_key=bdzQHUgRjaK9EEBmaTsTVv0S40x3QP9F";
-    const url='https://api.giphy.com/v1/gifs/random';
+    const url='https://api.giphy.com/v1/gifs/random'+"?"+apiKey;
 	
 	request = new XMLHttpRequest;
-	request.open('GET', url+"?"+apiKey+"&"+params, true);
+	request.open('GET', url+"&"+params, true);
 	
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
 			data = JSON.parse(request.responseText).data.image_url;
-			console.log(data);
-			document.getElementById("giphyme").innerHTML = '<img src = "'+data+'"  title="GIF via Giphy">';
+            console.log(request);
+            document.getElementById("giphyme").innerHTML = '<img src = "'+data+'"  title="GIF via Giphy">';
+			document.getElementById("giphyme").style.backgroundImage = "url("+data+")";
 		} else {
-			console.log('reached giphy, but API returned an error');
+            console.log('reached giphy, but API returned an error');
 		}
 	};
 
@@ -39,6 +40,18 @@ function displayGif(inputText) {
 
 document.addEventListener('DOMContentLoaded', displayGif);
 
+window.addEventListener('error', function(e){
+    if (errorCounter < 3) {
+        errorCounter++;
+        console.log(e);
+        displayGif();
+    } else {
+        alert("Invalid search term. Please try again.");
+        errorCounter = 0;
+        location.reload();
+    }
+    
+}, true);
 
 function checkEnter(e){
     if (e.keyCode === 13){
