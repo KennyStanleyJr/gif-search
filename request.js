a0f495a838450e2a727fcfb8e88deac4cbcd44ea
kpstanleyjr <kpstanleyjr@gmail.com>
2020-06-18 11:35:03 -0500

    The image finally stays in the frame

--- request.js
+++ request.js
@@ -1,9 +1,3 @@
-const input = document.getElementById("search-box");
-const header = document.getElementById("header");
-let headerHeight = 0;
-let contentHeight = 0;
-let imageHeight = 0;
-let imageWidth = 0;
 let q = '';
 let button = false;
 let errorCounter = 0;
@@ -13,51 +7,10 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-function containContent() {
-    headerHeight = document.getElementById("header").offsetHeight;
-    contentHeight = window.innerHeight - headerHeight - (5*vh);
-    document.documentElement.style.setProperty('--ch', `${contentHeight}px`);
-    // console.log("Content Height is: " + contentHeight);
-    // if (condition) {
-        
-    // }
-    if (document.querySelector('#giphyme img')) {
-        let gif = document.querySelector('#giphyme img');
-
-        imageHeight = gif.clientHeight;
-        imageWidth = gif.clientWidth;
-        // console.log("imageHeight + headerHeight > window.innerHeight: " + (imageHeight + headerHeight > window.innerHeight));
-        if ((imageWidth > window.innerWidth * 0.95) && ((imageHeight + headerHeight) < window.innerHeight)) {
-            console.log('portrait');
-            document.querySelector('#giphyme img').style.width = "100%";
-            document.querySelector('.content').style = "position: absolute;top: 50%;-ms-transform: translateY(-50%);transform: translateY(-50%);height: unset;width: 100%;text-align: center";
-            document.querySelector('header h1').style = "font-size: 5vmin;";
-            document.querySelector('.search-container').style = "width: unset;padding: 0 1rem;";
-            document.querySelector('#giphyme').style.height = "unset";
-        }
-        else {
-            console.log('landscape');
-            document.querySelector('#giphyme img').style.width = "unset";
-            document.querySelector('.content').style = "all: initial";
-            document.querySelector('.content').style = "height: 100%;padding: 5vh 0;";
-            document.querySelector('header h1').style = "font-size: unset";
-            document.querySelector('.search-container').style = "width: 80%;padding: unset";
-            document.querySelector('#giphyme').style.height = "var(--content-height)";
-            
-        }
-    }
-    
-}
-
-document.onreadystatechange = function() {
-    containContent();
-}; 
-
 window.addEventListener('resize', () => {
     // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-    containContent();
 });
 
 function displayGif(inputText) {
@@ -92,10 +45,13 @@ function displayGif(inputText) {
 	};
 
     request.send();
-    containContent();
 }
 
-document.addEventListener('DOMContentLoaded', displayGif);
+document.onreadystatechange = function() {
+    if (document.readyState === 'complete') {
+        displayGif();
+    }
+}; 
 
 window.addEventListener('error', function(e){
     if (errorCounter < 3) {
