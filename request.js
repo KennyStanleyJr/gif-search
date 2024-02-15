0a153047d1e89745cebbd232cae4ffc8cf0fce79
Kenny Stanley Jr <kpstanleyjr@gmail.com>
2024-02-14 18:11:16 -0600

    Updated to new api and importing api key now

--- request.js
+++ request.js
@@ -1,80 +1,90 @@
-let q = '';
-let button = false;
-let errorCounter = 0;
+import { apiKey } from './apiKey.js'
+
+let tag = ''
+let button = false
+let errorCounter = 0
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
 
 function displayGif(inputText) {
-    if (inputText) {
-        q = inputText;
-    }
+  if (inputText) {
+    tag = inputText
+  } else {
+    tag = ''
+  }
 
-    let params = "tag=" + q;
+  let params = 'tag=' + tag
 
-    if (button) {
-        params = "tag=" + document.getElementById("search-box").value;
-    }
-    
-    const apiKey = "api_key=bdzQHUgRjaK9EEBmaTsTVv0S40x3QP9F";
-    const url='https://api.giphy.com/v1/gifs/random'+"?"+apiKey;
-	
-	request = new XMLHttpRequest;
-	request.open('GET', url+"&"+params, true);
-	
-	request.onload = function() {
-		if (request.status >= 200 && request.status < 400){
-			data = JSON.parse(request.responseText).data.image_url;
-            console.log(request);
-            document.getElementById("giphyme").innerHTML = '<img src = "'+data+'"  title="GIF via Giphy">';
-		} else {
-            console.log('reached giphy, but API returned an error');
-		}
-	};
+  if (button) {
+    params = 'tag=' + document.getElementById('search-box')?.value
+  }
 
-	request.onerror = function() {
-		console.log('connection error');
-	};
+  const apiKeyString = 'api_key=' + apiKey
+  const url = 'https://api.giphy.com/v1/gifs/random' + '?' + apiKeyString
+  const urlWithParams = url + '&' + params
 
-    request.send();
-}
+  const request = new XMLHttpRequest()
+  request.open('GET', urlWithParams, true)
 
-document.onreadystatechange = function() {
-    if (document.readyState === 'complete') {
-        displayGif();
-    }
-}; 
-
-window.addEventListener('error', function(e){
-    if (errorCounter < 3) {
-        errorCounter++;
-        console.log(e);
-        displayGif();
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText)?.data
+      const url = data?.images?.original?.url
+      document.getElementById('giphyme').innerHTML =
+        '<img src = "' + url + '"  title="GIF via Giphy">'
     } else {
-        alert("Invalid search term. Please try again.");
-        errorCounter = 0;
-        location.reload();
+      console.log('reached giphy, but API returned an error')
     }
-    
-}, true);
+  }
 
-function checkEnter(e){
-    if (e.keyCode === 13){
-        button = false;
-        displayGif(document.getElementById("search-box").value);
-    }
+  request.onerror = function () {
+    console.log('connection error')
+  }
 
+  request.send()
 }
 
-function searchFromBtn() {
-    button = true;
-    displayGif(document.getElementById("search-box").value);
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    displayGif()
+  }
+}
+
+window.addEventListener(
+  'error',
+  function (e) {
+    if (errorCounter < 3) {
+      errorCounter++
+      console.log(e)
+      displayGif()
+    } else {
+      alert('Invalid search term. Please try again.')
+      errorCounter = 0
+      location.reload()
+    }
+  },
+  true
+)
+
+window.checkEnter = function checkEnter(e) {
+  if (e.keyCode === 13) {
+    button = false
+    const text = document.getElementById('search-box')?.value ?? ''
+    displayGif(text)
+  }
+}
+
+window.searchFromBtn = function searchFromBtn() {
+  button = true
+  const text = document.getElementById('search-box')?.value ?? ''
+  displayGif(text)
 }
